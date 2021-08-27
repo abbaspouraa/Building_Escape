ddb375eb30f73d8d7595a663d032ec32d136ec43
Amir Abbaspour <83620288+abbaspouraa@users.noreply.github.com>
2021-08-26 21:06:40 -0600

    Built a simple map

---
+++ Source/Building_Escape/OpenDoor.cpp
@@ -0,0 +1,34 @@
+// Copyright Amir Abbaspour 2021
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// FRotator CurrentRotation = GetOwner() -> GetActorRotation();
+	// GetOwner() -> SetActorRotation(CurrentRotation.Add(0, 90, 0));
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UE_LOG(LogTemp, Warning, TEXT("Text, %d %f %s"), intVar, floatVar, *fstringVar );
+}
+
